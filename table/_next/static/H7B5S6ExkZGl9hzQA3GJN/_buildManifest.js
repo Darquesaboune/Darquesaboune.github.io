b255d26b8c542d46cbc25f206168cbc9d1072e4d
Sayaka <alice@lolita.technology>
2022-10-26 20:36:41 +0200

    table update

--- table/_next/static/H7B5S6ExkZGl9hzQA3GJN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-2e05e7afbd2d3d81.js","static/chunks/717-2841c872374bd80e.js","static/chunks/pages/index-1a51a78490838778.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
